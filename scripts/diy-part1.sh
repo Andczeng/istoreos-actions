e1fcb2aba18c2e3fbb44841f115ba31c0eb1a54f
Andczeng <33886435+Andczeng@users.noreply.github.com>
2025-08-07 20:26:48 +0800

    Update diy-part1.sh

--- scripts/diy-part1.sh
+++ scripts/diy-part1.sh
@@ -21,5 +21,3 @@ echo 'src-git openclash https://github.com/vernesong/OpenClash' >>feeds.conf.def
 echo 'src-git adguardhome https://github.com/rufengsuixing/luci-app-adguardhome' >>feeds.conf.default
 # echo 'src-git mosdns https://github.com/sbwml/luci-app-mosdns' >>feeds.conf.default
 # echo 'src-git kiddin9 https://github.com/kiddin9/kwrt-packages' >>feeds.conf
-#qmodem
-echo 'src-git qmodem https://github.com/FUjr/QModem.git;main' >> feeds.conf
